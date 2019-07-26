0ff37831eed9170f63d26691b2f9b49fcfc67d68
Tino Kossmann <aiio@gmx.net>
2019-07-26 15:00:08 +0200

    add patch.sh script for automated patching and signing

--- patch.sh
+++ patch.sh
@@ -7,8 +7,11 @@ RED='\033[1;31m'
 WHITE='\033[1;37m'
 YELLOW='\033[1;33m'
 
-echo -e "${WHITE}Suche APK Datei 'com.freestylelibre.app.de_2019-04-22.apk' ...${NORMAL}"
-if [ -e APK/com.freestylelibre.app.de_2019-04-22.apk ]; then
+WORKDIR=$(pwd)
+FILENAME='com.freestylelibre.app.de_2019-04-22'
+
+echo -e "${WHITE}Suche APK Datei '${FILENAME}.apk' ...${NORMAL}"
+if [ -e APK/${FILENAME}.apk ]; then
   echo -e "${GREEN}  gefunden.${NORMAL}"
 else
   echo -e "${RED}  nicht gefunden.${NORMAL}"
@@ -18,7 +21,7 @@ else
 fi
 
 echo -e "${WHITE}Prüfe MD5 Summe der APK Datei ...${NORMAL}"
-md5sum -c APK/com.freestylelibre.app.de_2019-04-22.apk.md5 > /dev/null 2>&1
+md5sum -c APK/${FILENAME}.apk.md5 > /dev/null 2>&1
 if [ $? = 0 ]; then
   echo -e "${GREEN}  okay.${NORMAL}"
 else
@@ -29,7 +32,7 @@ else
 fi
 
 echo -e "${WHITE}Enpacke original APK Datei ...${NORMAL}"
-apktool d -o /tmp/librelink APK/com.freestylelibre.app.de_2019-04-22.apk
+apktool d -o /tmp/librelink APK/${FILENAME}.apk
 if [ $? = 0 ]; then
   echo -e "${GREEN}  okay.${NORMAL}"
 else
@@ -40,9 +43,8 @@ else
 fi
 
 echo -e "${WHITE}Patche original App ...${NORMAL}"
-echo $(pwd) > /tmp/librepatch.pwd
 cd /tmp/librelink/
-git apply $(cat /tmp/librepatch.pwd)/xdrip2.git.patch
+git apply --whitespace=nowarn --verbose ${WORKDIR}/xdrip2.git.patch
 if [ $? = 0 ]; then
   echo -e "${GREEN}  okay.${NORMAL}"
 else
@@ -53,7 +55,7 @@ else
 fi
 
 echo -e "${WHITE}Verwende neuen Sourcecode für gepatchte App ...${NORMAL}"
-cp -Rv $(cat /tmp/librepatch.pwd)/sources/* /tmp/librelink/smali_classes2/com/librelink/app/
+cp -Rv ${WORKDIR}/sources/* /tmp/librelink/smali_classes2/com/librelink/app/
 if [ $? = 0 ]; then
   echo -e "${GREEN}  okay.${NORMAL}"
 else
@@ -65,7 +67,7 @@ fi
 chmod 644 /tmp/librelink/smali_classes2/com/librelink/app/*.smali
 
 echo -e "${WHITE}Verwende neue Grafiken für gepatchte App ...${NORMAL}"
-cp -Rv $(cat /tmp/librepatch.pwd)/graphics/* /tmp/librelink/
+cp -Rv ${WORKDIR}/graphics/* /tmp/librelink/
 if [ $? = 0 ]; then
   echo -e "${GREEN}  okay.${NORMAL}"
 else
@@ -76,7 +78,7 @@ else
 fi
 
 echo -e "${WHITE}Kopiere original APK Datei in gepatchte App ...${NORMAL}"
-cp $(cat /tmp/librepatch.pwd)/APK/com.freestylelibre.app.de_2019-04-22.apk /tmp/librelink/assets/original.apk
+cp ${WORKDIR}/APK/${FILENAME}.apk /tmp/librelink/assets/original.apk
 if [ $? = 0 ]; then
   echo -e "${GREEN}  okay.${NORMAL}"
 else
@@ -87,7 +89,7 @@ else
 fi
 
 echo -e "${WHITE}Baue gepatchte App zusammen ...${NORMAL}"
-apktool b -o $(cat /tmp/librepatch.pwd)/APK/librelink_unaligned.apk
+apktool b -o ${WORKDIR}/APK/librelink_unaligned.apk
 if [ $? = 0 ]; then
   echo -e "${GREEN}  okay.${NORMAL}"
 else
@@ -98,13 +100,12 @@ else
 fi
 
 echo -e "${WHITE}Räume /tmp/ auf ...${NORMAL}"
-cd $(cat /tmp/librepatch.pwd)
+cd ${WORKDIR}
 rm -rf /tmp/librelink/
-rm /tmp/librepatch.pwd
 echo -e "${GREEN}  okay."
 
 echo -e "${WHITE}Optimiere Ausrichtung der gepatchten APK Datei...${NORMAL}"
-zipalign -p 4 APK/librelink_unaligned.apk APK/librelink_aligned.apk
+zipalign -p 4 APK/librelink_unaligned.apk APK/${FILENAME}_patched.apk
 if [ $? = 0 ]; then
   echo -e "${GREEN}  okay.${NORMAL}"
   rm APK/librelink_unaligned.apk
@@ -127,7 +128,7 @@ else
 fi
 
 echo -e "${WHITE}Signiere gepatchte APK Datei ...${NORMAL}"
-apksigner sign --ks-pass pass:geheim --ks /tmp/libre-keystore.jks APK/librelink_aligned.apk
+apksigner sign --ks-pass pass:geheim --ks /tmp/libre-keystore.jks APK/${FILENAME}_patched.apk
 if [ $? = 0 ]; then
   echo -e "${GREEN}  okay.${NORMAL}"
   rm /tmp/libre-keystore.jks
@@ -138,4 +139,4 @@ else
   exit 1
 fi
 
-echo -e "${GREEN}Fertig! Die fertig gepatchte und signierte APK Datei finden Sie unter APK/librelink_aligned.apk${NORMAL}"
+echo -e "${YELLOW}Fertig! Die fertig gepatchte und signierte APK Datei finden Sie unter APK/${FILENAME}_patched.apk${NORMAL}"
